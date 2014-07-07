bfedd8a2b63ad32d87aab7c7652100fd0d5607f3
Carlos Correia <MafaStudios@gmail.com>
2014-07-07 02:57:35 +0100

    correcções diversas

--- script.library.change/main.py
+++ script.library.change/main.py
@@ -27,31 +27,55 @@ def CATEGORIES():
 def changestreams():
 	nameoriginal = ''
 	nametochange = ''
+	count = 0
+	alreadychanged = False
 	dir,files = xbmcvfs.listdir(libraryfolder_change)
 	for directories in dir:
+		nametochange = ''
+		nameoriginal = ''
+		alreadychanged = False
 		libfolder = os.path.join(libraryfolder,directories)
 		libfolder_change = os.path.join(libraryfolder_change,directories)
+		ano=directories[len(directories)-5:-1]
+		titulo=directories[:-5]		
+		if ano:
+			yearbefore = int(ano)-1
+			yearafter = int(ano)+1
+			libfolder1 = os.path.join(libraryfolder,titulo+str(yearbefore)+')')
+			libfolder2 = os.path.join(libraryfolder,titulo+str(yearafter)+')')
+			if xbmcvfs.exists(libfolder1): libfolder = libfolder1
+			if xbmcvfs.exists(libfolder2): libfolder = libfolder2
 		if xbmcvfs.exists(libfolder):
-			for root2, dirs2, files2 in os.walk(libfolder_change):
-				for name2 in files2:
-					if '.strm' in name2:
-						nametochange = name2;
-			for root, dirs, files in os.walk(libfolder):
-				for name in files:
-					if '.strm' in name:
-						nameoriginal = name;					
-			if nametochange and nameoriginal:
-				xbmcvfs.rename(os.path.join(libfolder,nameoriginal),os.path.join(libfolder,nameoriginal[:-4]+'bak'))
-				xbmcvfs.copy(os.path.join(libfolder_change,nametochange),os.path.join(libfolder,nameoriginal))
-	ok = mensagemok('Trocar Streams','Processo terminado')
+			dir2,files2 = xbmcvfs.listdir(libfolder)				
+			for name2 in files2:
+				if '.strm' in name2:
+					nametochange = name2;
+				if '.bak' in name2:
+					alreadychanged = True;
+			if not alreadychanged:
+				dir3,files3 = xbmcvfs.listdir(libfolder_change)	
+				for name3 in files3:
+					if '.strm' in name3:
+						nameoriginal = name3;
+				if nametochange and nameoriginal:
+					count = count + 1
+					source = os.path.join(libfolder,nametochange)
+					destination = os.path.join(libfolder,nametochange[:-4]+'bak')
+					xbmcvfs.rename(source,destination)
+					shutil.copy(os.path.join(libfolder_change,nameoriginal),os.path.join(libfolder,nametochange))
+		else: print '##falhou'+libfolder
+	ok = mensagemok('Trocar Streams','Processo terminado\nForam alterados '+str(count)+' streams')
 
 def recoverbackups(path):
+	count = 0
 	for root, dirs, files in os.walk(path):
 		for name in files:
 			if '.bak' in name:
+				count = count + 1
 				fullpath = os.path.join(os.path.join(path,name[:-4]),name)
-				xbmcvfs.copy(fullpath,fullpath[:-3]+'strm')
+				shutil.copy(fullpath,fullpath[:-3]+'strm')
 				xbmcvfs.delete(fullpath)				
+	ok = mensagemok('Recuperar Streams','Processo terminado\nForam recuperados '+str(count)+' streams')
 	
 ######################################################FUNCOES JÁ FEITAS
 def addDir(name,mode,iconimage,pasta=False):
