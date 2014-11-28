7094f2acb3da70805a8d6c01bec40a0f58deacd1
Carlos Correia <MafaStudios@gmail.com>
2014-11-28 02:53:39 +0000

    fun videos 0.2.4
    Added break.com

--- plugin.video.funvideos/lib/util.py
+++ plugin.video.funvideos/lib/util.py
@@ -55,6 +55,9 @@ def getpages(id):
 			elif '9gag' in frame:
 				list2 = grab9gag(site+pageindex,prettyname,str(id+starton))
 				if list2: list.extend(list2)
+			elif 'break' in frame:
+				list2 = grabbreak(site+pageindex+str(id+starton),prettyname)
+				if list2: list.extend(list2)				
 			else:
 				startsection = parameters['startsection']
 				endsection = parameters['endsection']
@@ -182,6 +185,54 @@ def grab9gag(url,prettyname,id):
 		list.append(jsonloaded)
 	return list 
 
+def grabbreak(url,prettyname):
+	list = []
+	try:
+		page = open_url(url)
+		page = page.replace("\\","")
+		ids = re.findall('data-content-id="(\d+)"', page, re.DOTALL)
+		for videoid in ids:
+			videocache = os.path.join(cachePath,str(videoid))
+			if getSetting("cachesites") == 'true' and os.path.isfile(videocache):
+				jsonline = readfiletoJSON2(videocache)
+				jsonloaded = json.loads(jsonline, encoding="utf-8")
+				list.append(jsonloaded)
+			else:
+				content = open_url("http://www.break.com/embed/"+videoid)
+				matchAuth=re.compile('"AuthToken": "(.+?)"', re.DOTALL).findall(content)
+				matchURL=re.compile('"uri": "(.+?)".+?"height": (.+?),', re.DOTALL).findall(content)
+				matchYT=re.compile('"youtubeId": "(.*?)"', re.DOTALL).findall(content)
+				title=re.compile('"contentName": "(.+?)",', re.DOTALL).findall(content)
+				title = cleanTitle(title[0])
+				title2 = ''				
+				try: title2 = title.decode('utf8').encode('ascii','xmlcharrefreplace')
+				except: pass
+				if title2 <> '': title = title2		
+				duration=re.compile('"videoLengthInSeconds": "(\d+)",', re.DOTALL).findall(content)	
+				thumb = re.compile('"thumbUri": "(.+?)",', re.DOTALL).findall(content)				
+				finalUrl=""
+				if matchYT and matchYT[0]!="":
+					finalUrl = "plugin://plugin.video.youtube/?action=play_video&videoid=" + matchYT[0]
+					videocache2 = os.path.join(cachePath,str(matchYT[0]))				
+					if getSetting("cachesites") == 'true' and not os.path.isfile(videocache): 
+						jsontext = '{"prettyname":"'+prettyname+'","url":"'+finalUrl+'","title":"'+title+'","duration":"'+str(duration[0])+'","thumbnail":"'+thumb[0]+'"}'
+						jsonloaded = json.loads(jsontext, encoding="utf-8")			
+						writefile(videocache2,'w',jsontext.encode('utf8'))
+				else:
+					max=0
+					for url, height in matchURL:
+						height=int(height)
+						if height>max: 
+							finalUrl=url.replace(".wmv",".flv")+"?"+matchAuth[0]
+							max=height
+				jsontext = '{"prettyname":"'+prettyname+'","url":"'+finalUrl+'","title":"'+title+'","duration":"'+str(duration[0])+'","thumbnail":"'+thumb[0]+'"}'
+				jsonloaded = json.loads(jsontext, encoding="utf-8")
+				if getSetting("cachesites") == 'true' and not os.path.isfile(videocache): writefile(videocache,'w',jsontext.encode('utf8'))
+				list.append(jsonloaded)
+		if list: return list
+	except BaseException as e:
+		print '##ERROR-funvideos:Break_resolver: '+url+' '+str(e)
+
 def grabvit(url,prettyname):
 	list = []
 	try:
@@ -220,7 +271,7 @@ def sapo_resolver(url,prettyname):
 		if os.path.isfile(videocache):
 			jsonline = readfiletoJSON2(videocache)
 			jsonloaded = json.loads(jsonline, encoding="utf-8")
-			return jsonloaded,jsonloaded
+			return jsonline,jsonloaded
 		else:
 			try:
 				sapoAPI = open_url('http://rd3.videos.sapo.pt/'+match[0]+'/rss2')	
